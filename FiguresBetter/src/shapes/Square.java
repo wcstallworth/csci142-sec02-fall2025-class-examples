9ef9822da8b34978afa397f47fd5f2f09e3a9a5c
Will Stallworth <wstallworth@stetson.edu>
2025-09-08 16:18:24 -0400

    Working with abstract classes

---
+++ FiguresBetter/src/shapes/Square.java
@@ -0,0 +1,34 @@
+package shapes;
+
+import java.awt.Color;
+
+public class Square extends Shape {
+	private double sideLength; 			// <-- "property"
+	
+	public Square() {					// <-- "constructor"
+		this(1.0, Color.WHITE);
+	}
+	
+	public Square(double sideLength, Color color) {
+		super(color);
+		if (sideLength <= 0.0) {
+			this.sideLength = 1.0;
+		} else {
+			this.sideLength = sideLength;
+		}
+		
+	}
+
+	public double getSideLength() {
+		return sideLength;
+	}
+
+	public void setSideLength(double sideLength) {
+		this.sideLength = sideLength;
+	}
+
+	public double area() {
+		return this.sideLength * this.sideLength;
+	}
+
+}
